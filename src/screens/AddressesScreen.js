2c4a212bdd7567f05889a21fdeeb2abe977c82d5
AldG99 <a.garciaglz17@gmail.com>
2025-02-03 21:26:16 -0600

    Agregar nuevas pantallas con una estructura optimizada para una mejor experiencia de usuario

---
+++ src/screens/AddressesScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddressesScreen = () => {
+  return <div>AddressesScreen</div>;
+};
+
+export default AddressesScreen;
---
+++ src/screens/CoffeeDetailsScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CoffeeDetailsScreen = () => {
+  return <div>CoffeeDetailsScreen</div>;
+};
+
+export default CoffeeDetailsScreen;
